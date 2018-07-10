bb20f7465d9924beb5e859d9dcbe814b6d63658a
shivg7706 <shivamgupta7706@gmail.com>
2018-07-10 09:41:45 +0530

    complete board

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 
 # colours
 class Color:
+
     GREEN = '#0CED2C'
     RED = '#F71313'
     YELLOW = '#FFFF00'
@@ -8,8 +9,6 @@ class Color:
     DEFAULT = '#E9E9E9'
     CYAN = '#4EB1BA'
 
-
- 
 
 class Board:
 
@@ -19,8 +18,13 @@ class Board:
     BOARD_WIDTH = 640
     BOARD_HEIGHT = 640
     POINTS = [(0, 0), (0, 1), (1, 0), (1, 1)]
+    POSITIVE_V = [(6, 2), (8, 1), (6, 13), (8, 12)]
+    POSITIVE_H = [(1, 6), (2, 8), (13, 8), (12, 6)]
+
 
 class Text:
-    MADE_BY = 'Made By: Mansi Agrawal & Shivam Gupta '
+
+    MADE_BY = 'Made By: Mansi Agrawal & Shivam Gupta'
     HEADER =  'LUDO - THE GAME'
+
 
